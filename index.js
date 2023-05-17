4560bffccbbd680e908135a0a0d4a8fb034dec23
Jaron van Well <jaronvwell@gmail.com>
2023-05-17 17:11:09 +0200

    docker for milvus db and basic component template

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-const post = 3000 || process.env.PORT;
+const express = require('express');
+const PORT = 3000 || process.env.PORT;
 const app = express();
 
-// express basic setup
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send({ message: 'Hello World' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost://${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
