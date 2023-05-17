ec9c6488dbb3f9e9591bdf563ba1e257bc27e60d
Jaron van Well <jaronvwell@gmail.com>
2023-05-17 17:20:56 +0200

    basic setup for sqlite db

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const PORT = 3000 || process.env.PORT;
+const cors = require('cors')
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./sqlite/db.sqlite3');
+
 const app = express();
 
-
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello World' });
