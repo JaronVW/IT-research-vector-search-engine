4d81952a98abb875d65681837278a46b08b75714
Jaron van Well <jaronvwell@gmail.com>
2023-05-18 10:15:14 +0200

    Milvus connection works, readme added

---
+++ README.md
@@ -0,0 +1,23 @@
+# IT-research-vector-search-engine
+
+a school project for my IT research class
+
+## How to run
+
+ 1. install and run the docker container with the following command:
+
+    ```bash
+     docker compuse up -d
+    ```
+
+ 2. Run the nodejs server with the following command:
+
+    ```bash
+    node .
+    ```
+
+ 3. Open the index.html file in your browser
+
+## How to use
+
+This application has a search bar where you can type in a query. The query can be a single word or a sentence. There are two search buttons, one for the vector database and one for a relational Sqlite database. The goal of this project is to set up a minimal application using a vector database for a search/recommendation engine and compare it to a similar setup with a relational database. My findings during the development and testing of this application will bu documented in a blog post assigment.
--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const express = require('express');
-const PORT = 3000 || process.env.PORT;
-const cors = require('cors')
-const sqlite3 = require('sqlite3').verbose();
+import express from 'express';
+
+import cors from 'cors';
+import sqlite3 from 'sqlite3';
+import { MilvusClient } from '@zilliz/milvus2-sdk-node';
+import { DataType } from "@zilliz/milvus2-sdk-node";
+
+
+const PORT = process.env.PORT || 3000;
+const address = "localhost:19530" || process.env.MILVUS_ADDRESS;
+const username = "root" || process.env.MILVUS_USERNAME;
+const password = "Milvus" || process.env.MILVUS_PASSWORD;
+const ssl = false;
+
+const milvusClient = new MilvusClient(address, ssl, username, password);
+milvusClient.connect();
 const db = new sqlite3.Database('./sqlite/db.sqlite3');
 
-const app = express();
 
+const app = express();
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello World' });
 });
 
-app.listen(PORT, () => {
+app.get('/milvus', async (req, res) => {
+    milvusClient.showCollections().then((collections) => {
+        res.send({ collections });
+    });
+});
+
+app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
